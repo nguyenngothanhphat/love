6640e597b0b9d3094c910216a4f072fb9483b094
Thành Phát <thanhphat19@gmail.com>
2021-12-17 22:06:46 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Em yêu à!',
+    introDesc: `Em là một cô bé đáng yêu, dễ thương trong lòng anh. Dù em có như thế nào thì em vẫn là người đẹp nhất. Anh luôn muốn làm cho em luôn vui vẻ và hạnh phúc nhất`,
+    btnIntro: 'hihi',
+    title: 'Em là mặt trời của anh 😙',
+    desc: 'Em đã mang đến cho anh niềm vui, hạnh phúc',
+    btnYes: 'Vẫn cứ là thích anh <3',
+    btnNo: 'Không, Anh à :))))',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Vì em yêu anh <3',
+    mess: 'Anh biết nè 🥰. Anh yêu em nhiều lắm 😘😘',
+    messDesc: 'Tối noel 7h anh qua đón nhé công chúa nhỏ nhé.',
+    btnAccept: 'Dạ <3',
+    messLink: 'https://www.facebook.com/nguyenngothanhphat/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
